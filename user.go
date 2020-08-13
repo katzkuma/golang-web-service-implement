1c8a11a850878a9635ca8a908710333cd0cd434e
katzkuma <katzkuma@gmail.com>
2020-08-13 17:24:41 +0800

    feat: 新增 POST API 資料傳遞部分
    目前只完成資料傳遞，稍後增加資料庫存取功能。

--- user.go
+++ user.go
@@ -5,12 +5,63 @@ import (
 	"net/http"
 )
 
+type IsOK struct {
+	IsOK   bool    `json:"IsOK"`
+}
+
 func main() {
 	apiServer := gin.Default()
 
+	apiServer.POST("v1/user/create", create)
+	apiServer.POST("v1/user/delete", delete)
+	apiServer.POST("v1/user/pwd/change", pwd_change)
 	apiServer.GET("v1/user/login", login)
 
 	apiServer.Run(":8080")
+}
+
+func create(context *gin.Context) {
+	Account := context.PostForm("Account")
+	Password := context.PostForm("Password")
+
+	message := "ID:" + Account + ", pwd:" + Password
+	isOK := true
+	context.JSON(http.StatusOK, gin.H{
+		"Code":  0,
+		"message": message,
+		"Result": IsOK{
+			IsOK: isOK,
+		},
+	})
+}
+
+func delete(context *gin.Context) {
+	Account := context.PostForm("Account")
+
+	message := "ID:" + Account
+	isOK := true
+	context.JSON(http.StatusOK, gin.H{
+		"Code":  0,
+		"message": message,
+		"Result": IsOK{
+			IsOK: isOK,
+		},
+	})
+}
+
+func pwd_change(context *gin.Context) {
+	Account := context.PostForm("Account")
+	Password := context.PostForm("Password")
+
+	message := "ID:" + Account + ", pwd:" + Password
+	isOK := true
+	context.JSON(http.StatusOK, gin.H{
+		"Code":  0,
+		"message": message,
+		"Result": IsOK{
+			IsOK: isOK,
+		},
+	})
 }
 
 func login(context *gin.Context) {
